c8821bc9c737118f6ab616dee9aae7d1c5fea609
Daniel Wise <daniel@autosiliconehoses.com>
2018-08-07 11:51:37 +0100

    Added FPS Files to Repo
    Everything I've done so far.
    Will need Directory sorting out when adding a new Supplier.

---
+++ Scripts/ExportToBasket.ps1
@@ -0,0 +1,39 @@
+﻿$file = $args[0]
+$pipe = [RegEx]::Escape("|")
+$badIds = Import-CSV $file | Select 'SaleID' | ? {$_.SaleId | Select-String $pipe}
+$export = Import-CSV $file | Select 'SaleID', 'SellerID','Item SKU','Item Qty' | ? {!($_.SaleId | Select-String $pipe)}
+
+$badIds | % {
+    $badid = "Bad OrderId: " + $_.SaleId
+    $badid | Add-Content '\\DISKSTATION\Feeds\Ordering\Errors.txt'
+}
+
+$export | % {
+    $orderId = $_.SaleId
+    If ($orderId -match '\d\d\d-\d\d\d\d\d\d\d-\d\d\d\d\d\d\d') {
+        $amazon = $true
+    }
+    If (!($orderId -match '\d\d\d-\d\d\d\d\d\d\d-\d\d\d\d\d\d\d')) {
+        $ebay = $true
+        $sellerId = $_.SellerId
+        Switch ($sellerId) {
+            'autoperformanceonline' {$seller = 'APO'}
+            'ultimatecareparts' {$seller = 'UCP'}
+            'performanceraceparts' {$seller = 'PRP'}
+            'aptoolstore' {$seller = 'APT'}
+            'mrsilicone' {$seller = 'MRS'}
+            'autosiliconehoses_outlet' {$seller = 'ASHO'}
+            'apmotorstoreoutlet' {$seller = 'APM'}
+            'apautomotive509-1' {$seller = 'AUT'}
+            'AP Motor Store' {$seller = 'AMZ'}
+        }
+        $orderId = $orderId + $seller
+    }
+
+    $orderId
+    $orderPath = "\\DISKSTATION\Feeds\Ordering\Uploads\$orderId`.csv"
+    $sku = $_."Item SKU" -replace '(-...-FPS).*', ''
+    $qty = $_."Item Qty"
+    $order = "$sku,$qty"
+    $order | Add-Content $orderPath
+}
---
+++ Scripts/FPS_Upload.ps1
@@ -0,0 +1,88 @@
+﻿$Host.Ui.RawUI.WindowTitle = "FPS Ordering"
+$filepath = $args[0]
+$customerRef = $filepath.Replace('\\DISKSTATION\Feeds\Ordering\Uploads\','').Replace('.csv','')
+#$filepath = '\\DISKSTATION\Feeds\Ordering\Uploads\Test.csv'
+#$customerRef = 'YEET'
+
+$WatinPath = '\\DISKSTATION\Feeds\Ordering\Scripts\WatiN\bin\net40\WatiN.Core.dll'
+$watin = [Reflection.Assembly]::LoadFrom( $WatinPath )
+$ie = new-object WatiN.Core.IE("https://fdrive.fpsdistribution.co.uk/savedbasket/upload")
+
+#Login
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq "https://fdrive.fpsdistribution.co.uk/user/login") {
+    "Login Page"
+    gc "\\DISKSTATION\Feeds\Ordering\Scripts\login.txt" | % {Invoke-Expression $_}
+
+    $ie.TextField({param($fu) $fu.GetAttributeValue("name") -eq 'Username' }).Value = $username
+    $ie.TextField({param($fu) $fu.GetAttributeValue("name") -eq 'Password' }).Value = $password
+    $ie.Button({param($fu) $fu.GetAttributeValue("ClassName") -eq 'sign-in' }).ClickNoWait()
+}
+
+#Upload File
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq "https://fdrive.fpsdistribution.co.uk/savedbasket/upload") {
+    "Logged In"
+    Try {$ie.FileUpload({param($fu) $fu.GetAttributeValue("name") -eq 'file' }).set($filepath)}
+    Catch {"File not found"; $ie.Close(); Exit}
+    $ie.Button({param($fu) $fu.GetAttributeValue("ClassName") -eq 'btn btn-primary btn-block filepicker-load' }).Click()
+    "File Uploaded"
+}
+
+#Preview
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq "https://fdrive.fpsdistribution.co.uk/savedbasket/preview") {
+    "Preview Page"
+		$errorBool = $false
+    $ie.TableRows | % {
+        If ($_.ClassName -eq 'danger') {
+			$errorBool = $true
+			$errorMessage = $customerRef + " - " + $_.Text
+			$errorMessage
+			$error | Add-Content '\\DISKSTATION\Feeds\Ordering\Errors.txt'
+        }
+    }
+		If ($errorBool) {
+            $ie.Close()
+			Exit
+		}
+    $ie.Link({param($fu) $fu.GetAttributeValue("ClassName") -eq 'btn btn-primary purchaseorder-continue' }).Click()
+    $ie.Div({param($fu) $fu.GetAttributeValue("ClassName") -eq 'alert alert-info'}).InnerHtml
+    If ($ie.Div({param($fu) $fu.GetAttributeValue("ClassName") -eq 'alert alert-info'}).InnerHtml -like 'Your basket is currently empty') {$ie.Close(); EXIT}
+    "Continuing"
+}
+
+#Basket
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq "https://fdrive.fpsdistribution.co.uk/basket") {
+    "Proceeding with order"
+    $ie.Link({param($fu) $fu.GetAttributeValue("ClassName") -eq 'btn btn-primary btn-block' }).Click()
+}
+
+#Checkout
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq "https://fdrive.fpsdistribution.co.uk/order/details") {
+    "Filling in checkout form"
+    $ie.TextField({param($fu) $fu.GetAttributeValue("name") -eq 'customerref' }).Value = $customerRef
+    $ie.Button({param($fu) $fu.GetAttributeValue("ClassName") -eq 'btn btn-primary btn-block' }).Click()
+    "Form complete"
+}
+
+<#
+UNKNOWN TERRITORY
+REQUIRES AN ORDER TO BE MADE
+
+#Order Confirmation
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq <PUT URL HERE>) {
+    $ie.Button({param($fu) $fu.GetAttributeValue("ClassName") -eq <PUT BUTTON NAME HERE> }).Click()
+}
+
+#Print
+$ie.WaitForComplete()
+If ($ie.Uri.AbsoluteUri -eq <PUT URL HERE>) {
+    $ie.InternetExplorer.ExecWB(6,2,1)
+    $ie.Close()
+}
+
+#>
